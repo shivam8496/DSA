01b5b9f775c273857ccfd0528c4f60ceed34fabd
Shivam singh <shivamsingh8496@gmail.com>
2022-08-18 19:07:03 +0530

    New file added PrettyPrinting.java

---
+++ Lectures/lecture-1/java-tutorials/src/com/shivam/PrettyPrinting.java
@@ -0,0 +1,20 @@
+package com.shivam;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float s=10.4437f;
+        System.out.printf("%.2f",s);
+            /***    %c - Character
+                %d - Decimal number (base 10)
+                %e - Exponential floating-point number
+                %f - Floating-point number
+                %i - Integer (base 10)
+                %o - Octal number (base 8)
+                %s - String
+                %u - Unsigned decimal (integer) number
+                %x - Hexadecimal number (base 16)
+                %t - Date/time
+                %n - Newline
+             ***/
+    }
+}
