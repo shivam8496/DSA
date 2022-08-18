cd158c77ad5af380669119d3a8fd81f381a75897
Shivam singh <shivamsingh8496@gmail.com>
2022-08-18 19:35:07 +0530

    new file added Stringbuilder.java

--- Lectures/lecture-1/java-tutorials/src/com/shivam/codeTest.java
+++
@@ -1,8 +0,0 @@
-package com.shivam;
-
-public class codeTest {
-    public static void main(String[] args) {
-        int [] arr={1};
-        System.out.println(arr.length);
-    }
-}
---
+++ Lectures/lecture-1/java-tutorials/src/com/shivam/testCode.java
@@ -0,0 +1,12 @@
+package com.shivam;
+
+public class testCode {
+    public static void main(String[] args) {
+        String s= "shivam";
+        String b= "shivam";
+        b="shivam1";
+        System.out.println(b);
+
+
+    }
+}
