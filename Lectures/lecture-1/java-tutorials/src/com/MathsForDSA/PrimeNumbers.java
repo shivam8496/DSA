439aff7e14ff0019d72aac9a7985c9db99b11de1
Shivam singh <shivamsingh8496@gmail.com>
2022-08-23 12:18:39 +0530

    Code for PrimeNumbers.java Optimized

--- Lectures/lecture-1/java-tutorials/src/com/MathsForDSA/PrimeNumbers.java
+++ Lectures/lecture-1/java-tutorials/src/com/MathsForDSA/PrimeNumbers.java
@@ -2,18 +2,25 @@ package com.MathsForDSA;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        System.out.println(isPrime(1000));
+        System.out.println(isPrime(997));
     }
 
     static boolean isPrime(int n)
-    { int N=n;
-        if(n==2)
-        {return true;}
-        for (int i = 2; i <n ; i++) {
-            if(N%i==0)
-            {return false;}
+    { int N=2;
+        if(n==1)
+        {return false;}
+        while(N*N <=n)
+        {
+            if(n%N==0)
+            {
+                return false;
+            }
+            N++;
         }
         return true;
+
+        }
+
     }
 
-}
+
