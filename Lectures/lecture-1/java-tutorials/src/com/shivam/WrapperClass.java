e9a0c03f721379de8b4f68620dda2d8ca2b03e95
Shivam singh <shivamsingh8496@gmail.com>
2022-11-07 23:03:40 +0530

    New File added wrapperClass.java

---
+++ Lectures/lecture-1/java-tutorials/src/com/shivam/WrapperClass.java
@@ -0,0 +1,12 @@
+package com.shivam;
+
+public class WrapperClass {
+
+    public static void main(String[] args) {
+        Integer num = 123; // to Primitive data type into Object from 
+        int myInt = num.intValue(); //To again convert into Primitive valur
+        String str = num.toString(); //To Convert into string 
+        System.out.println(str);
+    }
+
+}
