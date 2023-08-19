86f8694f5abfbadd32a63ecd8c0d7636aa1eda6a
shiv. singh <Shivam singh>
2023-08-19 11:10:56 +0530

    New java class added

---
+++ com/recurrsionAgain/mergeSort.java
@@ -0,0 +1,65 @@
+package com.recurrsionAgain;
+
+import java.util.Arrays;
+
+public class mergeSort {
+    public static void main(String[] args) {
+     int [] arr ={1,4,2,6,9,10,15,67,23};
+     arr=mergeSort(arr);
+     System.out.println(Arrays.toString(arr));
+    }
+    static int [] mergeSort(int [] arr)
+    {
+        if(arr.length ==1)
+        {
+            return arr;
+        }
+
+        int mid= arr.length/2;
+
+        int [] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int [] right = mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return merge(left,right);
+    }
+
+     static int[] merge(int[] left, int[] right) {
+        int [] mix=new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j< right.length)
+        {
+            if (left[i]<right[j])
+            {
+                mix[k]=left[i];
+                i++;
+            }
+            else
+            {
+                mix[k]=right[j];
+                j++;
+            }
+            k++;
+
+        }
+
+        while (i<left.length)
+        {
+            mix[k]=left[i];
+            k++;
+            i++;
+        }
+
+         while (j<right.length)
+         {
+             mix[k]=right[j];
+             k++;
+             j++;
+         }
+
+        return mix;
+    }
+
+
+}
