d1d90e8b15b6784d7f4192f1a1d8ef7d04c32c7f
shiv. singh <Shivam singh>
2023-08-19 11:25:35 +0530

    Added new function in recurrsionAgain

--- com/recurrsionAgain/mergeSort.java
+++ com/recurrsionAgain/mergeSort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class mergeSort {
     public static void main(String[] args) {
-     int [] arr ={1,4,2,6,9,10,15,67,23};
-     arr=mergeSort(arr);
+     int [] arr ={5,4,3,2,1,0};
+     mergeSortInPlace(arr,0, arr.length);
      System.out.println(Arrays.toString(arr));
     }
     static int [] mergeSort(int [] arr)
@@ -61,5 +61,61 @@ public class mergeSort {
         return mix;
     }
 
+
+    static void mergeSortInPlace(int [] arr,int s,int e)
+    {
+        if(e-s == 1)
+        {
+            return ;
+        }
+
+        int mid= (s+e)/2;
+
+        mergeSortInPlace(arr,s,mid);
+        mergeSortInPlace(arr,mid,e);
+
+         mergeInPlace(arr,s,mid,e);
+    }
+
+    static void mergeInPlace(int [] arr,int s,int mid,int e) {
+        int [] mix=new int[e-s];
+        int i=s;
+        int j=mid;
+        int k=0;
+        while(i<mid && j< e)
+        {
+            if (arr[i]<arr[j])
+            {
+                mix[k]=arr[i];
+                i++;
+            }
+            else
+            {
+                mix[k]=arr[j];
+                j++;
+            }
+            k++;
+
+        }
+
+        while (i<mid)
+        {
+            mix[k]=arr[i];
+            k++;
+            i++;
+        }
+
+        while (j<e)
+        {
+            mix[k]=arr[j];
+            k++;
+            j++;
+        }
+
+        for(k=0;k< mix.length;k++)
+        {
+            arr[s+k]=mix[k];
+        }
+    }
 
 }
