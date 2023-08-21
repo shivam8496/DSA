fe974f9f1a78d817c193d7468767a7717a67d6fc
shiv. singh <Shivam singh>
2023-08-21 22:26:26 +0530

    changes Made

--- com/Recurrsion_Subset_subsequence_String/SubSequence.java
+++ com/Recurrsion_Subset_subsequence_String/SubSequence.java
@@ -1,4 +1,22 @@
 package com.Recurrsion_Subset_subsequence_String;
 
 public class SubSequence {
+    public static void main(String[] args) {
+        System.out.println(seq("","Shivam",0));
+
+    }
+
+    static int seq(String p,String up,int count)
+    {
+        if(up.isEmpty())
+        {
+            System.out.println(p);
+            count++;
+            return count ;
+        }
+
+        return seq(p+up.charAt(0),up.substring(1),count) + seq(p,up.substring(1),count);
+
+
+    }
 }
