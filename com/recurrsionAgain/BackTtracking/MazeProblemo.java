4b305b10beee217bf021657520d158ad016c9a4e
shiv. singh <Shivam singh>
2023-08-27 23:47:43 +0530

    changes made

--- com/recurrsionAgain/BackTtracking/MazeProblemo.java
+++ com/recurrsionAgain/BackTtracking/MazeProblemo.java
@@ -1,4 +1,25 @@
 package com.recurrsionAgain.BackTtracking;
 
 public class MazeProblemo {
+    public static void main(String[] args) {
+      int up=3;
+      int p=3;
+        System.out.println(Maze(up,p));
+    }
+
+
+    static int Maze(int p,int up)
+    {
+        if(p==0 || up==0)
+        {
+            System.out.println(p+" "+up);
+            return 1 ;
+        }
+        int count=0;
+        count=count+Maze(p-1,up)+Maze(p,up-1);;
+
+        return count;
+    }
+
+
 }
