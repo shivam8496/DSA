f152cd48830909c4ab87935e98d360a2b5b24278
shiv. singh <Shivam singh>
2023-08-28 00:19:42 +0530

    chaned made

--- com/recurrsionAgain/BackTtracking/MazeProblemo.java
+++ com/recurrsionAgain/BackTtracking/MazeProblemo.java
@@ -4,13 +4,13 @@ public class MazeProblemo {
     public static void main(String[] args) {
       int up=3;
       int p=3;
-        System.out.println(Maze(up,p));
+        System.out.println(MazeDiagonalPath("",up,p));
     }
 
 
     static int Maze(int p,int up)
     {
-        if(p==0 || up==0)
+        if(p==1 || up==1)
         {
             System.out.println(p+" "+up);
             return 1 ;
@@ -20,6 +20,27 @@ public class MazeProblemo {
 
         return count;
     }
+
+    static int MazePath(String path,int p,int up)
+    {
+        if(p==1 && up==1)
+        {
+            System.out.println(path);
+            return 1 ;
+        }
+        int c=0;
+        int r=0;
+        if(p>1)
+        { r=MazePath(path+"D",p-1,up);}
+
+        if(up>1) {
+             c = MazePath(path + "R", p, up - 1);
+        }
+     return r+c;
+    }
+
+    
+
 
 
 }
