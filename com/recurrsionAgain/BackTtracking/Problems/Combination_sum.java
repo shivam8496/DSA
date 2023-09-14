0b8237713e0030151a590797e22fe035a170ff26
shiv. singh <Shivam singh>
2023-09-14 11:34:53 +0530

    Problems folder added

---
+++ com/recurrsionAgain/BackTtracking/Problems/Combination_sum.java
@@ -0,0 +1,47 @@
+package com.recurrsionAgain.BackTtracking.Problems;
+
+// Problem Link(medium) - https://leetcode.com/problems/combination-sum/description/
+
+import java.util.ArrayList;
+
+public class Combination_sum {
+
+    public static void main(String[] args) {
+        int [] candidates ={2,3,5};
+        int target =8;
+        ArrayList<Integer> ans=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> answer0=new ArrayList<>();
+         ArrayList<ArrayList<Integer>> answer=solution(candidates,8,0,0,ans,answer0);
+
+
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println(answer);
+        }
+
+    }
+
+      static ArrayList<ArrayList<Integer>> solution(int [] arr,int t,int s,int index,ArrayList<Integer> ans, ArrayList<ArrayList<Integer>> answer)
+    {   if(s>t)
+        {
+            return answer ;
+        }
+        if(s==t)
+        {
+            System.out.println(ans);
+            answer.add(ans);
+            return answer;
+        }
+        for (int i = index; i < arr.length; i++) {
+            ans.add(arr[i]);
+            s=s+arr[i];
+            solution(arr,t,s,i,ans,answer);
+            ans.remove(ans.size()-1);
+            s=s-arr[i];
+
+        }
+
+        return answer;
+
+    }
+
+}
