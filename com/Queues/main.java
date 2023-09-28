106caca41907d5e1f309f4c50fe5909c976ea5b8
shiv. singh <Shivam singh>
2023-09-28 11:25:59 +0530

    Modified QUEUES Folder

--- com/Queues/main.java
+++ com/Queues/main.java
@@ -4,16 +4,16 @@ package com.Queues;
 
 public class main {
     public static void main(String[] args) {
-        CustomQueus Q=new CustomQueus(5);
+        CirculerQueue Q=new CirculerQueue(5);
         Q.add(1);
         Q.add(2);
         Q.add(3);
         Q.add(4);
         Q.add(5);
-//        Q.display();
-         Q.remove();
-        Q.remove();
-        Q.remove();
+        Q.display();
+        System.out.println( Q.remove());
+        System.out.println( Q.remove());
+        System.out.println( Q.remove());
 
         Q.display();
     }
