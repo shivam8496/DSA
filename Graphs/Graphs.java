c501b73aac0e81f153f5dd5e969186133f2a812a
Shivam singh <79458284+shivam8496@users.noreply.github.com>
2024-07-22 00:01:26 +0530

    BFS and DFS code added

--- Graphs/Graphs.java
+++ Graphs/Graphs.java
@@ -118,6 +118,19 @@ public class Graphs {
             }
         }
     }
+
+    public static void DFS(List<Edge> [] graph,int curr, boolean [] visited)
+    {
+        System.out.print(curr+" ");
+        visited[curr]=true;
+        for(int i=0;i<graph[curr].size();i++)
+        {
+            Edge e=graph[curr].get(i);
+            if(visited[e.dest]==false){
+                DFS(graph,e.dest,visited);
+            }
+        }
+    }
     public static void main(String [] args){
         int V=7;
         List<Edge> [] graph=new ArrayList[V];
@@ -133,7 +146,7 @@ public class Graphs {
                 BFS(graph,visited,i);
             }
         }
-
+        DFS(graph,0,visited);
     }
 
 }
