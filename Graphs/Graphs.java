8de7093efdc817c8bf9c0516f92282bbb58c51be
Shivam singh <79458284+shivam8496@users.noreply.github.com>
2024-07-21 15:37:05 +0530

    Changes made in graphs

--- Graphs/Graphs.java
+++ Graphs/Graphs.java
@@ -26,17 +26,26 @@ public class Graphs {
         {
             graph[i]=new ArrayList<>();
         }
+        graph[0].add(new Edge(0,1));
         graph[0].add(new Edge(0,2));
 
-        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,0));
         graph[1].add(new Edge(1,3));
 
         graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,3));
-        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,4));
 
         graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,4));
+        graph[3].add(new Edge(3,5));
+
+        graph[4].add(new Edge(4,2));
+        graph[4].add(new Edge(4,3));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5,6));
     }
     public static void createWeightedGraph(List<Edge> [] graph)
     {
@@ -88,12 +97,43 @@ public class Graphs {
             System.out.println();
         }
     }
+
+
+    public static  void BFS(List<Edge> [] graph,boolean [] visited , int start)
+    {
+     Queue<Integer> q=new LinkedList<>();
+
+     q.add(start);
+        while (!q.isEmpty()) {
+            int curr=q.remove();
+            if(visited[curr]==false)
+            {
+                System.out.print(curr);
+                visited[curr]=true;
+                for(int i=0;i<graph[curr].size();i++)
+                {
+                    Edge e=graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
     public static void main(String [] args){
-        int V=4;
+        int V=7;
         List<Edge> [] graph=new ArrayList[V];
-//        createGraph(graph);
-        createWeightedGraph(graph);
-        printAllNeighbours(graph);
+        createGraph(graph);
+//        createWeightedGraph(graph);
+//        printAllNeighbours(graph);
+
+        boolean [] visited = new boolean[graph.length];
+        for(int i=0;i<visited.length;i++)
+        {
+            if(visited[i]==false)
+            {
+                BFS(graph,visited,i);
+            }
+        }
+
     }
 
 }
